7c29e2534537d8618cbef56d0202a16ae6e88df0
daniellanza4 <143770451+daniellanza4@users.noreply.github.com>
2026-01-31 21:01:20 -0600

    Descripción clara del cambio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserDashboard } from './pages/user-dashboard/user-dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'usuario', component: UserDashboard },
+  { path: '', redirectTo: 'usuario', pathMatch: 'full' }
+];
---
+++ src/app/pages/user-dashboard/user-dashboard.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-user-dashboard',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './user-dashboard.html',
+  styleUrl: './user-dashboard.css'
+})
+export class UserDashboard {
+
+}
