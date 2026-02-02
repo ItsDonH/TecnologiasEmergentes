fa7ac9b533ecfb5772e1ee7c43c52e90c0b8ea11
ItsDonH <heberlopez282@gmail.com>
2026-02-02 17:55:57 -0600

    pantalla de admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { UserDashboard } from './pages/user-dashboard/user-dashboard';
+import { AdminComponent } from './pages/admin/admin.component';
 
 export const routes: Routes = [
   { path: 'usuario', component: UserDashboard },
+  { path: 'admin', component: AdminComponent},
   { path: '', redirectTo: 'usuario', pathMatch: 'full' }
 ];
