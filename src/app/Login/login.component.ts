eb7b431a4bab18beb33297455af133a3af69ac2f
Artga26 <Andreortega267@gmail.com>
2026-02-03 19:15:16 -0600

    Login

---
+++ src/app/Login/login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.components.html',
+  styleUrls: ['./login.components.css']
+})
+export class LoginComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Login/login.component';
 import { UserDashboard } from './pages/user-dashboard/user-dashboard';
 import { AdminComponent } from './pages/admin/admin.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'usuario', component: UserDashboard },
   { path: 'admin', component: AdminComponent},
-  { path: '', redirectTo: 'usuario', pathMatch: 'full' }
+  { path: '**', redirectTo: 'login' }
 ];
