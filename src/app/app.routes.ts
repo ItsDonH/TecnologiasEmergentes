ff5ba9a434ebaf7ed452d5b765d7288f24cb0555
Buevo <133726073+Buevo@users.noreply.github.com>
2026-02-03 19:25:15 -0600

    Login (visual)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './Login/login.component';
+import { LoginComponent } from './pages/Login/login.component';
 import { UserDashboard } from './pages/user-dashboard/user-dashboard';
 import { AdminComponent } from './pages/admin/admin.component';
 
